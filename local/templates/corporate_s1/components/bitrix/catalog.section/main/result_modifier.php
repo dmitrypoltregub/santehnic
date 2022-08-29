4425f7312a46b5e0d23900bf6e5a5efcb3d3e2a3
/usr/bin/vim <support@beget.ru>
2022-08-29 13:11:54 +0300

    dev2908-2

--- local/templates/corporate_s1/components/bitrix/catalog.section/main/result_modifier.php
+++ local/templates/corporate_s1/components/bitrix/catalog.section/main/result_modifier.php
@@ -1,8 +1,8 @@
 <? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
-$arUF = $GLOBALS["USER_FIELD_MANAGER"]->GetUserFields("IBLOCK_22_SECTION", $arResult['ID'],"UF_TOP_DESCRIPTION");
-if($arUF["UF_PAGE_HEADER"]["VALUE"] != ""){
+$rsResult = CIBlockSection::GetList(array("SORT" => "ASC"), array("IBLOCK_ID" => "22", "ID" => $arResult['ID']), false, $arSelect = array("UF_*"));
+if($secResult = $rsResult -> GetNext()){
 
-    $arResult['UF_TOP_DESCRIPTION'] = $arUF["UF_TOP_DESCRIPTION"];
+    $arResult['UF_TOP_DESCRIPTION'] = $secResult["UF_TOP_DESCRIPTION"];
 
 }
